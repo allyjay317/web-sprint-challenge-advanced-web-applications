b3bbfedf7c94853a5d3cce4c71c23ba103dd4d78
Allison Usher <allyjay317@gmail.com>
2020-08-14 18:11:23 -0700

    blocked out login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <label htmlFor='username'>
+          Username:
+          <input type='text' name='username' value={credentials.username} onChange={handleChange} label='username' />
+        </label>
+        <label htmlFor='password'>
+          Password:
+          <input type='text' name='password' value={credentials.password} onChange={handleChange} label='password' />
+        </label>
+      </form>
     </>
   );
 };
